16a9b093287cade319d05b14989bfc7bd74c626e
anunezmartinez <ahriannm00@gmail.com>
2022-08-07 06:07:20 +0200

    Initial Commit

---
+++ Neith/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Neith
+//
+//  Created by Adrian Nuñez Martíez on 7/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Neith/NeithApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeithApp.swift
+//  Neith
+//
+//  Created by Adrian Nuñez Martíez on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct NeithApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
