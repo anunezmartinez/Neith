f0b15755b4d33f4882a902178e04938f11e10dcf
anunezmartinez <ahriannm00@gmail.com>
2022-09-07 22:05:29 +0200

    Splash Screen created.

---
+++ Neith/Model/SplashScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SplashScreen.swift
+//  Neith
+//
+//  Created by Adrian Nuñez Martíez on 7/9/22.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreen()
+    }
+}
---
+++ Neith/View/SplashScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashScreenView.swift
+//  Neith
+//
+//  Created by Adrian Nuñez Martíez on 7/9/22.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
